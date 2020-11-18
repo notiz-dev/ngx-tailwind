b0e5ad9639b85ef309e0e798c7cd8c2a00c13ecb
Marc Stammerjohann <marcstammerjohann@web.de>
2020-11-18 22:06:45 +0100

    chore(tailwind): add purge sample

--- angular-workspace/tailwind.config.js
+++ angular-workspace/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['src/app/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
