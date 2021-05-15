823b9991bf66cce77983c31e90eb5bd6034bd01a
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-05-15 17:07:09 +0200

    add tailwind

---
+++ angular-workspace/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
